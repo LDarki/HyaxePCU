822c2faccfb15b3c4767d5068faa2a573f931886
DarkThinking <serverdarkcity@gmail.com>
2020-08-17 22:02:00 -0300

    Contenido del head pasado a include

---
+++ views/inc/head.php
@@ -0,0 +1,9 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" crossorigin="anonymous">
+
+<meta name="description" content="Hyaxe V | PCU">
+<meta name="author" content="LDarki">
+
+<title>Hyaxe V | PCU</title>
--- views/index.php
+++ views/index.php
@@ -2,15 +2,9 @@
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" crossorigin="anonymous">
-
-    <meta name="description" content="Hyaxe V | PCU">
-    <meta name="author" content="LDarki">
-
-    <title>Hyaxe V | PCU</title>
+    <?php 
+    require_once("./views/inc/head.php");
+    ?>
 
     <link rel="stylesheet" href="./assets/css/font-awesome.css">
     <link rel="stylesheet" href="./assets/css/main.css">
